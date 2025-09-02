6ce04a4453774a51640ce7d424d4b24ebc220fa5
Kaiyrkul Kanatbekova <kanatbekova_ka@auca.kg>
2025-09-02 12:04:14 +0600

    Complete lab 1. Added working SQL script.

---
+++ lab1.sql
@@ -0,0 +1,15 @@
+
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    age INT
+);
+
+INSERT INTO students (name, age)
+VALUES ('Alice', 21), ('Bob', 23);
+
+SELECT * FROM students;
+
+
